5e276ce4011b3b187e1f93720973d9a27f233b70
shjosa <shjosapak@gmail.com>
2022-05-29 17:09:27 -0400

    enable adding items

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const objArr = [{name: "eat", key: 0}, {name: "play elden ring :)", key: 1}, {name: "walk dogs", key: 2}, {name: "read", key: 3}]
+  const [objArr, setObjArr] = useState([{name: "eat", key: 0}, {name: "play elden ring :)", key: 1}, {name: "walk dogs", key: 2}, {name: "read", key: 3}])
+  // todo: useRef()
+  let [id, setId] = useState(4);
+
+  function addToArray() {
+    const newId = id + 1;
+    setObjArr([...objArr, {key: newId, name: "new todo"}]);
+    setId(newId);
+  }
+
   return (
-    <ul>
-      {objArr.map((obj, i) => <li key={obj.key}>
-        {obj.name}
-      </li>)}
-    </ul>
+    <div>
+      <button onClick={addToArray}>Add Item</button>
+      <ul>
+        {objArr.map((obj, i) => <li key={obj.key}>
+          {obj.name}
+        </li>)}
+      </ul>
+    </div>
   )
 }
 
