0137cfccc60b4137afd87dfb42efe6f6cc43edc2
shjosa <shjosapak@gmail.com>
2022-06-01 19:55:16 -0400

    added ability to add custom tasks

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,33 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [objArr, setObjArr] = useState([{name: "eat", key: 0}, {name: "play elden ring :)", key: 1}, {name: "walk dogs", key: 2}, {name: "read", key: 3}])
+  const [objArr, setObjArr] = useState([{name: "eat", key: 0}, {name: "play elden ring :)", key: 1}, {name: "walk dogs", key: 2}, {name: "read", key: 3}]);
+  const [task, setTask] = useState("");
   // todo: useRef()
-  let [id, setId] = useState(4);
+  const [id, setId] = useState(4);
 
-  function addToArray() {
+  function addToArray(taskName: string) {
     const newId = id + 1;
-    setObjArr([...objArr, {key: newId, name: "new todo"}]);
+    setObjArr([...objArr, {key: newId, name: taskName}]);
     setId(newId);
+  }
+
+  const onChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setTask(e.target.value);
+  }
+
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    addToArray(task);
+    setTask("");
   }
 
   return (
     <div>
-      <button onClick={addToArray}>Add Item</button>
+      <form onSubmit={onSubmit}>
+        <input type="text" onChange={onChange} value={task} />
+        <button type="submit">Add Item</button>
+      </form>
       <ul>
         {objArr.map((obj, i) => <li key={obj.key}>
           {obj.name}
