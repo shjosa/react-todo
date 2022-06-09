fa1228e4ace380c6c8566316693ce217c95fe92a
shjosa <shjosapak@gmail.com>
2022-06-09 12:18:38 -0400

    replaced basic task adding features with rough MUI elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState, useMemo, useRef } from 'react';
+import { AppBar, Toolbar, Fab, Modal, Box, Typography, TextField } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
 import './App.css';
 import { TodoList } from './components/todo-list';
 import { Task, getTaskList, setTaskList } from './utils/api';
@@ -6,6 +9,7 @@ import { Task, getTaskList, setTaskList } from './utils/api';
 function App() {
   const [objArr, setObjArr] = useState<Array<Task>>([]);
   const [task, setTask] = useState("");
+  const [open, setOpen] = useState(false);
   const id = useRef(0);
 
   const activeTodo = useMemo(() => objArr.filter(task => !task.isCompleted), [objArr]);
@@ -59,12 +63,56 @@ function App() {
     setTask("");
   }
 
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
   return (
     <div>
+      <AppBar color="secondary">
+        <Toolbar>
+          <Fab color="warning" onClick={handleOpen}>
+            <EditIcon />
+          </Fab>
+        </Toolbar>
+      </AppBar>
+      <Modal
+        keepMounted
+        open={open}
+        onClose={handleClose}
+      >
+        <Box 
+          sx={style}
+          component="form"
+          onSubmit={onSubmit}
+        >
+          <Typography>Add a new item</Typography>
+          <TextField fullWidth onChange={onChange} value={task}></TextField>
+          <Fab color="primary" type="submit">
+            <AddIcon />
+          </Fab>
+        </Box>
+      </Modal>
+      <br/>
+      <br/>
+      <br/>
+      {/*
       <form onSubmit={onSubmit}>
         <input type="text" onChange={onChange} value={task} />
         <button type="submit">Add Item</button>
       </form>
+      */}
       <TodoList taskList={activeTodo} sectionName="Active" toggleCompleted={toggleCompleted} removeFromArray={removeFromArray}/>
       <TodoList taskList={completedTodo} sectionName="Completed" toggleCompleted={toggleCompleted} removeFromArray={removeFromArray}/>
     </div>
--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -8,16 +8,16 @@ interface TodoListProps {
 }
   
 export function TodoList({ taskList, sectionName, toggleCompleted, removeFromArray }: TodoListProps) {
-if (!taskList.length)
-    return null;
-return <>
-    <h1>{sectionName}</h1>
-    <ul>
-    {taskList.map((obj, i) => <li key={obj.key}>
-        <input type="checkbox" onChange={() => toggleCompleted(obj.key)} checked={obj.isCompleted} />
-        {obj.name}
-        <button onClick={() => removeFromArray(obj.key)}>Remove</button>
-    </li>)}
-    </ul>
-</>
+    if (!taskList.length)
+        return null;
+    return <>
+        <h1>{sectionName}</h1>
+        <ul>
+            {taskList.map((obj, i) => <li key={obj.key}>
+                <input type="checkbox" onChange={() => toggleCompleted(obj.key)} checked={obj.isCompleted} />
+                {obj.name}
+                <button onClick={() => removeFromArray(obj.key)}>Remove</button>
+            </li>)}
+        </ul>
+    </>
 }
